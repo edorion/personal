b1d7ea2a77920558a6b84a43ebf442c7031957ca
Andrew McDonald <andrew.mcdonald@sportsbet.com.au>
2014-04-30 17:22:23 +1000

    ex1-4

---
+++ HardWay/x3/x3.rb
@@ -0,0 +1,26 @@
+# prints test
+puts "I will now count my chickens:"
+
+#25+(30/6)
+puts "Hens", 25 + 30 / 6
+#100 - (((25 * 3) / 100) * 4)
+puts "Roosters", 100 - 25 * 3.0 % 4
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4.0 % 2.0 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2/", 3 + 2
+puts "What is 5 - 7", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal to?", 5 <= -2
